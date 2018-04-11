0e46aa47abaab33fb7a5ccbff70666e9497efb14
Gennaro <g.anzalone12.com>
2018-04-11 14:41:35 -0700

    create pending players migration

---
+++ db/migrate/20180411213606_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
